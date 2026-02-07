ebb603883595f3d14f68387b822b545a6095b4a4
neeteshdixit <dixitneetesh857@gmail.com>
2026-02-08 00:35:56 +0530

    no change

---
+++ Request_and_Response.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+function requestListener(req,res){
+    console.log(req);
+}
+
+const server = http.createServer((req,res)=>{
+    console.log(req.url,req.method,req.headers);
+    // process.exit();
+});
+
+const PORT = 3000;
+server.listen(PORT, ()=>{
+    console.log(' server running at http://localhost:${PORT}');
+});
