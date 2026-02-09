f9f6c0cd657906794b1a1905042cf5acb327d5b8
neeteshdixit <dixitneetesh857@gmail.com>
2026-02-09 21:40:28 +0530

    write response

--- Request_and_Response.js
+++ Request_and_Response.js
@@ -6,6 +6,12 @@ function requestListener(req,res){
 
 const server = http.createServer((req,res)=>{
     console.log(req.url,req.method,req.headers);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>Complete node</title></head>');
+    res.write('<body><h1>Read</h1></body>')
+    res.write('</html>')
+    res.end();
     // process.exit();
 });
 
@@ -13,3 +19,4 @@ const PORT = 3000;
 server.listen(PORT, ()=>{
     console.log(' server running at http://localhost:${PORT}');
 });
+ 
